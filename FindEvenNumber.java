4f8f1de884293f740717b9b870ee32e6f37c4860
vadirajaav <94743059+vadirajaav@users.noreply.github.com>
2022-02-25 20:19:33 +0530

    Add files via upload

---
+++ FindEvenNumber.java
@@ -0,0 +1,19 @@
+package week1.day2;
+
+public class FindEvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=5;
+		
+		if (num % 2 == 0) {
+			System.out.println(" Given number is even" + num);
+				}
+		else {
+			System.out.println("Given number is odd: " + num);
+		}
+
+	}
+
+}
